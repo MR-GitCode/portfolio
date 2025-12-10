96b2690cd3d00dd051fc84575d65cbfc2d8d14d6
Michael Ring <michael.ring.87@web.de>
2025-12-10 11:03:43 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsOverviewComponent } from './projects-overview/projects-overview.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsOverviewComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/header/toggle-language/toggle-language.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-language',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle-language.component.html',
+  styleUrl: './toggle-language.component.scss'
+})
+export class ToggleLanguageComponent {
+
+}
