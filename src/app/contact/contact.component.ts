06452f4333d02212d5c8c5cbaf484265ddff6ab1
Michael Ring <michael.ring.87@web.de>
2025-12-18 23:32:26 +0100

    html + css contact section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormComponent,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
