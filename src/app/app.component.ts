94cdc3498cd0f7242fe69663bf1355f5032d401f
Michael Ring <michael.ring.87@web.de>
2026-01-20 12:43:02 +0100

    fixed css of main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-root',
@@ -15,4 +18,7 @@ import { HeaderComponent } from './shared/header/header.component';
 })
 export class AppComponent {
   title = 'portfolio';
+  private readonly router = inject(Router);
+  private readonly viewportScroller = inject(ViewportScroller)
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
@@ -10,7 +10,13 @@ import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      })
+    ),
     provideClientHydration(),
     provideHttpClient(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
